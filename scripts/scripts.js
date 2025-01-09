3c7b037459b70a8e7c4ddeb6c223a92b65b3d91b
shabnam rezapour <sh.rezpapour88@gmail.com>
2025-01-09 16:20:00 +0300

    bug fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,12 +19,11 @@ import {
  */
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
-  const p = main.querySelector('p');
   const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1, p] }));
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
   }
 }
